8ea7b67e28a28723774d5e8aad4b75f04e27d25c
IBLR <216284997+IBLR@users.noreply.github.com>
2026-01-24 15:45:37 +0100

    Add header + burger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log("Test")
+/*** HEADER - Opening & closing of the burger menu ***/
+  const navToggleBtn = document.querySelector('.header-nav-toggle-btn button');
+  const nav = document.querySelector('#primary-navigation');
+  const firstNavLink = nav.querySelector('a');
+
+  navToggleBtn.addEventListener('click', () => {
+    const menuIsOpen = nav.classList.contains('is-open');
+    if (menuIsOpen) {
+      nav.classList.remove('is-open');
+      navToggleBtn.setAttribute('aria-expanded', 'false');
+      navToggleBtn.focus();
+    } else {
+      nav.classList.add('is-open');
+      navToggleBtn.setAttribute('aria-expanded', 'true');
+      firstNavLink.focus();
+    }
+  });
